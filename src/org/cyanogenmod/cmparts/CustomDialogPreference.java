1b3debd8b293c64ddcd9daed966dcd39d275f17b
Steve Kondik <steve@cyngn.com>
2016-09-08 02:15:37 -0700

    cmparts: Add a few more helpers for dialogs
    Change-Id: Id671328f83184ced213fd6fada41333ef1ec2db1

--- src/org/cyanogenmod/cmparts/CustomDialogPreference.java
+++ src/org/cyanogenmod/cmparts/CustomDialogPreference.java
@@ -20,12 +20,13 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v14.preference.PreferenceDialogFragment;
 import android.support.v7.preference.DialogPreference;
 import android.util.AttributeSet;
 import android.view.View;
 
-public class CustomDialogPreference extends DialogPreference {
+public class CustomDialogPreference<T extends Dialog> extends DialogPreference {
 
     private CustomPreferenceDialogFragment mFragment;
 
@@ -50,8 +51,8 @@ public class CustomDialogPreference extends DialogPreference {
         return getDialog() != null && getDialog().isShowing();
     }
 
-    public Dialog getDialog() {
-        return mFragment != null ? mFragment.getDialog() : null;
+    public T getDialog() {
+        return (T) (mFragment != null ? mFragment.getDialog() : null);
     }
 
     protected void onPrepareDialogBuilder(AlertDialog.Builder builder,
@@ -65,6 +66,14 @@ public class CustomDialogPreference extends DialogPreference {
     }
 
     protected void onBindDialogView(View view) {
+    }
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return null;
+    }
+
+    protected View onCreateDialogView(Context context) {
+        return null;
     }
 
     private void setFragment(CustomPreferenceDialogFragment fragment) {
@@ -107,6 +116,26 @@ public class CustomDialogPreference extends DialogPreference {
         public void onClick(DialogInterface dialog, int which) {
             super.onClick(dialog, which);
             getCustomizablePreference().onClick(dialog, which);
+        }
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            getCustomizablePreference().setFragment(this);
+            final Dialog sub = getCustomizablePreference().onCreateDialog(savedInstanceState);
+            if (sub == null) {
+                return super.onCreateDialog(savedInstanceState);
+            }
+            return sub;
+        }
+
+        @Override
+        protected View onCreateDialogView(Context context) {
+            final View v = getCustomizablePreference().onCreateDialogView(context);
+            if (v == null) {
+                return super.onCreateDialogView(context);
+            }
+            return v;
         }
     }
 }
