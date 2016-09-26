362d0d69e70629dce691ae8ab452e8eb3d46138b
Steve Kondik <steve@cyngn.com>
2016-09-25 23:53:46 -0700

    cmparts: Add contributors cloud
     * Original work by:
       - Jorge Ruesga
       - Danesh Mondegarian
       - Matt Mower
       - Michael Bestas
    
    Change-Id: Iddb528af063e75fdea58627903bed9a8f18fed29

---
+++ src/org/cyanogenmod/cmparts/BootReceiver.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cyanogenmod.cmparts;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.preference.PreferenceManager;
+
+import org.cyanogenmod.cmparts.contributors.ContributorsCloudFragment;
+import org.cyanogenmod.cmparts.input.ButtonSettings;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "BootReceiver";
+    private static final String ONE_TIME_TUNABLE_RESTORE = "hardware_tunable_restored";
+
+    @Override
+    public void onReceive(Context ctx, Intent intent) {
+        if (!hasRestoredTunable(ctx)) {
+            /* Restore the hardware tunable values */
+            ButtonSettings.restoreKeyDisabler(ctx);
+            setRestoredTunable(ctx);
+        }
+
+        // Extract the contributors database
+        ContributorsCloudFragment.extractContributorsCloudDatabase(ctx);
+    }
+
+    private boolean hasRestoredTunable(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getBoolean(ONE_TIME_TUNABLE_RESTORE, false);
+    }
+
+    private void setRestoredTunable(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        preferences.edit().putBoolean(ONE_TIME_TUNABLE_RESTORE, true).apply();
+    }
+}
