71981e94c4d2537eb2108e02b35c74eac6ef510c
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-10-14 18:44:12 +0800

    CMParts: applications: Add Expanded Desktop settings
    Original work by:
     - Kyrylo Mikos
     - Dave Kover
     - Clark Scheff
     - Adnan Begovic
    
    This consists of the following Settings commits from CM 13.0:
    dd6f7452d4bc5e6a4cb6497d48ed61e3f1d1faba Settings: Squashed commit for expanded desktop
    fd3eb78710e902a9c7d48af069cde0b03bba26bc Break out status bar icon from extdesk icon.
    2794f4fe351fd96ce04b6d8ce4854a3362ac88ef Settings: Make sure to rebuild on app entries loaded.
    
    Change-Id: I5ae9080cf19962c38899c90e1e0288835e2043d4

---
+++ src/org/cyanogenmod/cmparts/applications/ExpandedDesktopSettings.java
@@ -0,0 +1,584 @@
+/**
+ * Copyright (C) 2015-2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cyanogenmod.cmparts.applications;
+
+import android.annotation.Nullable;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManagerGlobal;
+import android.view.WindowManagerPolicyControl;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SectionIndexer;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.android.settingslib.applications.ApplicationsState;
+
+import org.cyanogenmod.cmparts.PartsActivity;
+import org.cyanogenmod.cmparts.R;
+import org.cyanogenmod.cmparts.SettingsPreferenceFragment;
+import org.cyanogenmod.cmparts.widget.SwitchBar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ExpandedDesktopSettings extends SettingsPreferenceFragment
+        implements AdapterView.OnItemClickListener, ApplicationsState.Callbacks,
+        SwitchBar.OnSwitchChangeListener {
+
+    private static final String EXPANDED_DESKTOP_PREFERENCE_TAG = "expanded_desktop_prefs";
+
+    private static final int STATE_DISABLED = 0;
+    private static final int STATE_STATUS_HIDDEN = 1;
+    private static final int STATE_NAVIGATION_HIDDEN = 2;
+    private static final int STATE_BOTH_HIDDEN = 3;
+
+    private AllPackagesAdapter mAllPackagesAdapter;
+    private ApplicationsState mApplicationsState;
+    private ApplicationsState.Session mSession;
+    private ActivityFilter mActivityFilter;
+    private Map<String, ApplicationsState.AppEntry> mEntryMap =
+            new HashMap<String, ApplicationsState.AppEntry>();
+
+    private SwitchBar mSwitchBar;
+    private ListView mUserListView;
+
+    private boolean mIsGloballyExpanded;
+
+    private boolean isGloballyExpanded(ContentResolver cr) {
+        final String value = Settings.Global.getString(cr, Settings.Global.POLICY_CONTROL);
+        if ("immersive.full=*".equals(value)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mApplicationsState = ApplicationsState.getInstance(getActivity().getApplication());
+        mSession = mApplicationsState.newSession(this);
+        mSession.resume();
+        mActivityFilter = new ActivityFilter(getActivity().getPackageManager());
+
+        mIsGloballyExpanded = isGloballyExpanded(getActivity().getContentResolver());
+        if (!mIsGloballyExpanded) {
+            WindowManagerPolicyControl.reloadFromSetting(getActivity(),
+                    Settings.Global.POLICY_CONTROL);
+        }
+        mAllPackagesAdapter = new AllPackagesAdapter(getActivity());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.expanded_desktop_layout, container, false);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        if (mSwitchBar != null) {
+            mSwitchBar.removeOnSwitchChangeListener(this);
+        }
+    }
+
+    @Override
+    public void onViewCreated(final View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        transactFragment();
+
+        mUserListView = (ListView) view.findViewById(R.id.user_list_view);
+        mUserListView.setAdapter(mAllPackagesAdapter);
+        mUserListView.setOnItemClickListener(this);
+
+        mSwitchBar = ((PartsActivity) getActivity()).getSwitchBar();
+        mSwitchBar.addOnSwitchChangeListener(this);
+        mSwitchBar.setChecked(mIsGloballyExpanded);
+        mSwitchBar.show();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        rebuild();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        save();
+        mSession.pause();
+        mSession.release();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        ViewHolder holder = (ViewHolder) view.getTag();
+        holder.mode.performClick();
+    }
+
+    @Override
+    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+        if (isChecked) {
+            enableForAll();
+        } else {
+            userConfigurableSettings();
+        }
+    }
+
+    @Override
+    public void onPackageListChanged() {
+        mActivityFilter.updateLauncherInfoList();
+        rebuild();
+    }
+
+    @Override
+    public void onRebuildComplete(ArrayList<ApplicationsState.AppEntry> entries) {
+        rebuild();
+    }
+
+    @Override
+    public void onLoadEntriesCompleted() {
+        rebuild();
+    }
+
+    @Override
+    public void onAllSizesComputed() {}
+
+    @Override
+    public void onLauncherInfoChanged() {}
+
+    @Override
+    public void onPackageIconChanged() {}
+
+    @Override
+    public void onPackageSizeChanged(String packageName) {}
+
+    @Override
+    public void onRunningStateChanged(boolean running) {}
+
+    private void writeValue(String value) {
+        Settings.Global.putString(getContentResolver(), Settings.Global.POLICY_CONTROL, value);
+    }
+
+    private static int getStateForPackage(String packageName) {
+        int state = STATE_DISABLED;
+
+        if (WindowManagerPolicyControl.immersiveStatusFilterMatches(packageName)) {
+            state = STATE_STATUS_HIDDEN;
+        }
+
+        if (WindowManagerPolicyControl.immersiveNavigationFilterMatches(packageName)) {
+            if (state == STATE_DISABLED) {
+                state = STATE_NAVIGATION_HIDDEN;
+            } else {
+                state = STATE_BOTH_HIDDEN;
+            }
+        }
+
+        return state;
+    }
+
+    private void enableForAll() {
+        mIsGloballyExpanded = true;
+        writeValue("immersive.full=*");
+        mAllPackagesAdapter.notifyDataSetInvalidated();
+        showGlobalUi();
+    }
+
+    private void userConfigurableSettings() {
+        mIsGloballyExpanded = false;
+        writeValue("");
+        WindowManagerPolicyControl.reloadFromSetting(getActivity());
+        mAllPackagesAdapter.notifyDataSetInvalidated();
+        showPerAppUi();
+    }
+
+    private void showGlobalUi() {
+        mUserListView.setVisibility(View.GONE);
+        showFragment();
+    }
+
+    private void showPerAppUi() {
+        hideFragment();
+        mUserListView.setVisibility(View.VISIBLE);
+    }
+
+    private void showFragment() {
+        final FragmentManager manager = getChildFragmentManager();
+        manager.beginTransaction()
+                .show(manager.findFragmentByTag(EXPANDED_DESKTOP_PREFERENCE_TAG))
+                .commit();
+    }
+
+    private void hideFragment() {
+        final FragmentManager manager = getChildFragmentManager();
+        manager.beginTransaction()
+                .hide(manager.findFragmentByTag(EXPANDED_DESKTOP_PREFERENCE_TAG))
+                .commit();
+    }
+
+    private void transactFragment() {
+        final Fragment fragment = ExpandedDesktopPrefs.newInstance();
+        getChildFragmentManager().beginTransaction()
+                .replace(R.id.expanded_desktop_prefs, fragment, EXPANDED_DESKTOP_PREFERENCE_TAG)
+                .commit();
+    }
+
+    private void handleAppEntries(List<ApplicationsState.AppEntry> entries) {
+        final ArrayList<String> sections = new ArrayList<String>();
+        final ArrayList<Integer> positions = new ArrayList<Integer>();
+        final PackageManager pm = getPackageManager();
+        String lastSectionIndex = null;
+        int offset = 0;
+
+        for (int i = 0; i < entries.size(); i++) {
+            final ApplicationInfo info = entries.get(i).info;
+            final String label = (String) info.loadLabel(pm);
+            final String sectionIndex;
+
+            if (!info.enabled) {
+                sectionIndex = "--"; // XXX
+            } else if (TextUtils.isEmpty(label)) {
+                sectionIndex = "";
+            } else {
+                sectionIndex = label.substring(0, 1).toUpperCase();
+            }
+
+            if (lastSectionIndex == null ||
+                    !TextUtils.equals(sectionIndex, lastSectionIndex)) {
+                sections.add(sectionIndex);
+                positions.add(offset);
+                lastSectionIndex = sectionIndex;
+            }
+
+            offset++;
+        }
+
+        mAllPackagesAdapter.setEntries(entries, sections, positions);
+        mEntryMap.clear();
+        for (ApplicationsState.AppEntry e : entries) {
+            mEntryMap.put(e.info.packageName, e);
+        }
+
+        if (mIsGloballyExpanded) {
+            showGlobalUi();
+        } else {
+            showPerAppUi();
+        }
+    }
+
+    private void rebuild() {
+        final ArrayList<ApplicationsState.AppEntry> newEntries = mSession.rebuild(
+                mActivityFilter, ApplicationsState.ALPHA_COMPARATOR);
+        if (newEntries != null) {
+            handleAppEntries(newEntries);
+            mAllPackagesAdapter.notifyDataSetChanged();
+        }
+    }
+
+    private void save() {
+        if (!mIsGloballyExpanded) {
+            WindowManagerPolicyControl.saveToSettings(getActivity(),
+                    Settings.Global.POLICY_CONTROL);
+        }
+    }
+
+    private int getStateDrawable(int state) {
+        switch (state) {
+            case STATE_STATUS_HIDDEN:
+                return R.drawable.ic_expdesk_hide_statusbar;
+            case STATE_NAVIGATION_HIDDEN:
+                return R.drawable.ic_expdesk_hide_navbar;
+            case STATE_BOTH_HIDDEN:
+                return R.drawable.ic_expdesk_hide_both;
+            case STATE_DISABLED:
+            default:
+                return R.drawable.ic_expdesk_hide_none;
+        }
+    }
+
+    private class AllPackagesAdapter extends BaseAdapter
+            implements AdapterView.OnItemSelectedListener, SectionIndexer {
+
+        private final LayoutInflater mInflater;
+        private final ModeAdapter mModesAdapter;
+        private List<ApplicationsState.AppEntry> mEntries = new ArrayList<>();
+        private String[] mSections;
+        private int[] mPositions;
+
+        public AllPackagesAdapter(Context context) {
+            mInflater = LayoutInflater.from(context);
+            mModesAdapter = new ModeAdapter(context);
+            mActivityFilter = new ActivityFilter(context.getPackageManager());
+        }
+
+        @Override
+        public int getCount() {
+            return mEntries.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mEntries.get(position);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return mEntries.get(position).id;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                holder = new ViewHolder(mInflater.inflate(
+                        R.layout.expanded_desktop_list_item, parent, false));
+                holder.mode.setAdapter(mModesAdapter);
+                holder.mode.setOnItemSelectedListener(this);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            ApplicationsState.AppEntry entry = mEntries.get(position);
+
+            if (entry == null) {
+                return holder.rootView;
+            }
+
+            holder.title.setText(entry.label);
+            mApplicationsState.ensureIcon(entry);
+            holder.icon.setImageDrawable(entry.icon);
+            holder.mode.setSelection(getStateForPackage(entry.info.packageName), false);
+            holder.mode.setTag(entry);
+            holder.stateIcon.setImageResource(getStateDrawable(
+                    getStateForPackage(entry.info.packageName)));
+            return holder.rootView;
+        }
+
+        private void setEntries(List<ApplicationsState.AppEntry> entries,
+                List<String> sections, List<Integer> positions) {
+            mEntries = entries;
+            mSections = sections.toArray(new String[sections.size()]);
+            mPositions = new int[positions.size()];
+            for (int i = 0; i < positions.size(); i++) {
+                mPositions[i] = positions.get(i);
+            }
+            notifyDataSetChanged();
+        }
+
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            final ApplicationsState.AppEntry entry = (ApplicationsState.AppEntry) parent.getTag();
+
+            WindowManagerPolicyControl.removeFromWhiteLists(entry.info.packageName);
+            switch (position) {
+                case STATE_STATUS_HIDDEN:
+                    WindowManagerPolicyControl.addToStatusWhiteList(entry.info.packageName);
+                    break;
+                case STATE_NAVIGATION_HIDDEN:
+                    WindowManagerPolicyControl.addToNavigationWhiteList(entry.info.packageName);
+                    break;
+                case STATE_BOTH_HIDDEN:
+                    WindowManagerPolicyControl.addToStatusWhiteList(entry.info.packageName);
+                    WindowManagerPolicyControl.addToNavigationWhiteList(entry.info.packageName);
+                    break;
+            }
+            save();
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {}
+
+        @Override
+        public int getPositionForSection(int section) {
+            if (section < 0 || section >= mSections.length) {
+                return -1;
+            }
+
+            return mPositions[section];
+        }
+
+        @Override
+        public int getSectionForPosition(int position) {
+            if (position < 0 || position >= getCount()) {
+                return -1;
+            }
+
+            final int index = Arrays.binarySearch(mPositions, position);
+
+            /*
+             * Consider this example: section positions are 0, 3, 5; the supplied
+             * position is 4. The section corresponding to position 4 starts at
+             * position 3, so the expected return value is 1. Binary search will not
+             * find 4 in the array and thus will return -insertPosition-1, i.e. -3.
+             * To get from that number to the expected value of 1 we need to negate
+             * and subtract 2.
+             */
+            return index >= 0 ? index : -index - 2;
+        }
+
+        @Override
+        public Object[] getSections() {
+            return mSections;
+        }
+    }
+
+    private static class ViewHolder {
+        private TextView title;
+        private Spinner mode;
+        private ImageView icon;
+        private View rootView;
+        private ImageView stateIcon;
+
+        private ViewHolder(View view) {
+            this.title = (TextView) view.findViewById(R.id.app_name);
+            this.mode = (Spinner) view.findViewById(R.id.app_mode);
+            this.icon = (ImageView) view.findViewById(R.id.app_icon);
+            this.stateIcon = (ImageView) view.findViewById(R.id.state);
+            this.rootView = view;
+
+            view.setTag(this);
+        }
+    }
+
+    private static class ModeAdapter extends BaseAdapter {
+
+        private final LayoutInflater inflater;
+        private boolean hasNavigationBar = true;
+        private final int[] items = {
+                R.string.expanded_desktop_style_hide_nothing,
+                R.string.expanded_desktop_style_hide_status,
+                R.string.expanded_desktop_style_hide_navigation,
+                R.string.expanded_desktop_style_hide_both
+        };
+
+        private ModeAdapter(Context context) {
+            inflater = LayoutInflater.from(context);
+
+            try {
+                hasNavigationBar = WindowManagerGlobal.getWindowManagerService().hasNavigationBar();
+            } catch (RemoteException e) {
+                // Do nothing
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return hasNavigationBar ? 4 : 2;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView view;
+            if (convertView != null) {
+                view = (TextView) convertView;
+            } else {
+                view = (TextView) inflater.inflate(android.R.layout.simple_spinner_dropdown_item,
+                        parent, false);
+            }
+
+            view.setText(items[position]);
+            view.setTextColor(0xFF757575);
+            view.setTextSize(14f);
+
+            return view;
+        }
+    }
+
+    private class ActivityFilter implements ApplicationsState.AppFilter {
+
+        private final PackageManager mPackageManager;
+        private final List<String> mLauncherResolveInfoList = new ArrayList<String>();
+
+        private ActivityFilter(PackageManager packageManager) {
+            this.mPackageManager = packageManager;
+
+            updateLauncherInfoList();
+        }
+
+        public void updateLauncherInfoList() {
+            Intent i = new Intent(Intent.ACTION_MAIN);
+            i.addCategory(Intent.CATEGORY_LAUNCHER);
+            List<ResolveInfo> resolveInfoList = mPackageManager.queryIntentActivities(i, 0);
+
+            synchronized (mLauncherResolveInfoList) {
+                mLauncherResolveInfoList.clear();
+                for (ResolveInfo ri : resolveInfoList) {
+                    mLauncherResolveInfoList.add(ri.activityInfo.packageName);
+                }
+            }
+        }
+
+        @Override
+        public void init() {}
+
+        @Override
+        public boolean filterApp(ApplicationsState.AppEntry entry) {
+            boolean show = !mAllPackagesAdapter.mEntries.contains(entry.info.packageName);
+            if (show) {
+                synchronized (mLauncherResolveInfoList) {
+                    show = mLauncherResolveInfoList.contains(entry.info.packageName);
+                }
+            }
+            return show;
+        }
+    }
+}
