05599930211a906a7fa874db7dae0a4c706286a1
Sam Mortimer <sam@mortimer.me.uk>
2017-09-06 23:14:02 -0700

    IntervalSeekBar attrs min,max -> minValue,maxValue
    To avoid conflict with:
    frameworks/support/v7/preference/res/values/attrs.xml:280
    
    Change-Id: Ic57d6296e2514431462a07b1dc5a56985a9eccb7

--- src/org/lineageos/lineageparts/widget/IntervalSeekBar.java
+++ src/org/lineageos/lineageparts/widget/IntervalSeekBar.java
@@ -40,8 +40,8 @@ public class IntervalSeekBar extends SeekBar {
         TypedArray seekBarType = context.obtainStyledAttributes(attrs,
                 R.styleable.IntervalSeekBar, 0, 0);
 
-        mMax = seekBarType.getFloat(R.styleable.IntervalSeekBar_max, 1.5f);
-        mMin = seekBarType.getFloat(R.styleable.IntervalSeekBar_min, 0.5f);
+        mMax = seekBarType.getFloat(R.styleable.IntervalSeekBar_maxValue, 1.5f);
+        mMin = seekBarType.getFloat(R.styleable.IntervalSeekBar_minValue, 0.5f);
         mDefault = seekBarType.getFloat(R.styleable.IntervalSeekBar_defaultValue, 1.0f);
 
         int digits = seekBarType.getInt(R.styleable.IntervalSeekBar_digits, 0);
