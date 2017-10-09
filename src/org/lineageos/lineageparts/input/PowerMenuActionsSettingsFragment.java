673f46582f207753cec2110677fdfbd5156a54e4
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-08 21:54:50 -0400

    parts: Move powermenu to settings fragment
    Change-Id: I00c39f20263cdb942e8ae7f13e2334c4b05500ef

--- src/org/lineageos/lineageparts/input/PowerMenuActionsSettingsFragment.java
+++ src/org/lineageos/lineageparts/input/PowerMenuActionsSettingsFragment.java
@@ -17,6 +17,7 @@
 
 package org.lineageos.lineageparts.input;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.UserInfo;
@@ -24,13 +25,14 @@ import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
+import android.support.v14.preference.PreferenceFragment;
 import android.support.v7.preference.CheckBoxPreference;
 import android.support.v7.preference.Preference;
+import android.view.MenuItem;
 
 import com.android.internal.util.lineage.PowerMenuConstants;
 
 import org.lineageos.lineageparts.R;
-import org.lineageos.lineageparts.SettingsPreferenceFragment;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -49,7 +51,7 @@ import static com.android.internal.util.lineage.PowerMenuConstants.GLOBAL_ACTION
 import static com.android.internal.util.lineage.PowerMenuConstants.GLOBAL_ACTION_KEY_USERS;
 import static com.android.internal.util.lineage.PowerMenuConstants.GLOBAL_ACTION_KEY_VOICEASSIST;
 
-public class PowerMenuActions extends SettingsPreferenceFragment {
+public class PowerMenuActionsSettingsFragment extends PreferenceFragment {
     final static String TAG = "PowerMenuActions";
 
     private CheckBoxPreference mRebootPref;
@@ -69,10 +71,10 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
     private String[] mAllActions;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.power_menu_settings);
+        final ActionBar actionBar = getActivity().getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
         mContext = getActivity().getApplicationContext();
 
         mAvailableActions = getActivity().getResources().getStringArray(
@@ -224,6 +226,15 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            getActivity().onBackPressed();
+            return true;
+        }
+        return false;
+    }
+
     private boolean settingsArrayContains(String preference) {
         return mLocalUserConfig.contains(preference);
     }
@@ -249,7 +260,7 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
     }
 
     private void updatePreferences() {
-        boolean bugreport = Settings.Global.getInt(getContentResolver(),
+        boolean bugreport = Settings.Global.getInt(mContext.getContentResolver(),
                 Settings.Global.BUGREPORT_IN_POWER_MENU, 0) != 0;
 
         if (mBugReportPref != null) {
@@ -301,7 +312,7 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
             }
         }
 
-        LineageSettings.Secure.putStringForUser(getContentResolver(),
+        LineageSettings.Secure.putStringForUser(mContext.getContentResolver(),
                 LineageSettings.Secure.POWER_MENU_ACTIONS, s.toString(), UserHandle.USER_CURRENT);
         updatePowerMenuDialog();
     }
