12dcf0cb6adfac1a62aa8ea94bb4cc420961233a
LuK1337 <priv.luk@gmail.com>
2018-01-08 15:05:56 +0100

    LineageParts: Don't disable whole home button category
    * Most of these settings still work despite
      having software navigation bar enabled.
    
    Change-Id: I13760438a1a7e6690c90c119b710408aa5f39d6c

--- src/org/lineageos/lineageparts/input/ButtonSettings.java
+++ src/org/lineageos/lineageparts/input/ButtonSettings.java
@@ -210,13 +210,6 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         mNavigationHomeDoubleTapAction = initList(KEY_NAVIGATION_HOME_DOUBLE_TAP,
                 homeDoubleTapAction);
 
-        // Hide navigation bar home settings if we have a hardware home key
-        // so that action config options aren't duplicated.
-        if (hasHomeKey) {
-                mNavigationPreferencesCat.removePreference(mNavigationHomeLongPressAction);
-                mNavigationPreferencesCat.removePreference(mNavigationHomeDoubleTapAction);
-        }
-
         final LineageHardwareManager hardware = LineageHardwareManager.getInstance(getActivity());
 
         // Only visible on devices that does not have a navigation bar already,
@@ -266,6 +259,10 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
 
             mHomeLongPressAction = initList(KEY_HOME_LONG_PRESS, homeLongPressAction);
             mHomeDoubleTapAction = initList(KEY_HOME_DOUBLE_TAP, homeDoubleTapAction);
+            if (mDisableNavigationKeys.isChecked()) {
+                mHomeLongPressAction.setEnabled(false);
+                mHomeDoubleTapAction.setEnabled(false);
+            }
 
             hasAnyBindableKey = true;
         } else {
@@ -564,8 +561,25 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         */
 
         /* Toggle hardkey control availability depending on navbar state */
+        if (mNavigationPreferencesCat != null) {
+            if (navbarEnabled) {
+                mNavigationPreferencesCat.addPreference(mNavigationHomeLongPressAction);
+                mNavigationPreferencesCat.addPreference(mNavigationHomeDoubleTapAction);
+            } else {
+                mNavigationPreferencesCat.removePreference(mNavigationHomeLongPressAction);
+                mNavigationPreferencesCat.removePreference(mNavigationHomeDoubleTapAction);
+            }
+        }
         if (homeCategory != null) {
-            homeCategory.setEnabled(!navbarEnabled);
+            if (mHomeAnswerCall != null) {
+                mHomeAnswerCall.setEnabled(!navbarEnabled);
+            }
+            if (mHomeLongPressAction != null) {
+                mHomeLongPressAction.setEnabled(!navbarEnabled);
+            }
+            if (mHomeDoubleTapAction != null) {
+                mHomeDoubleTapAction.setEnabled(!navbarEnabled);
+            }
         }
         if (backCategory != null) {
             backCategory.setEnabled(!navbarEnabled);
