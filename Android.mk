720fa59d89bb1b1bda0fd993d84c29d4e2aa8f26
Joey <joey@lineageos.org>
2018-01-20 13:54:57 +0100

    parts: introduce berry styles
    Change-Id: Ifaacc78eed5234cdbe2fd089a06524bdf1193c8f
    Signed-off-by: Joey <joey@lineageos.org>

--- Android.mk
+++ Android.mk
@@ -8,6 +8,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4 \
     android-support-v13 \
     android-support-v7-recyclerview \
+    android-support-v7-palette \
     android-support-v7-preference \
     android-support-v7-appcompat \
     android-support-v14-preference \
