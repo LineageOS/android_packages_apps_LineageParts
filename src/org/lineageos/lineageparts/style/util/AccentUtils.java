4a87d93f29da618a8465fa8d856f449fe5884831
Joey <joey@lineageos.org>
2018-02-22 16:52:47 +0100

    LineageParts: switch to styles api
    TODO:
    
    * Strip out style detection from wallpaper from parts into sdk
    
    Change-Id: I3a8497505bf8f7def59654beec92dd2ebfdd057c
    Signed-off-by: Joey <joey@lineageos.org>

--- src/org/lineageos/lineageparts/style/util/AccentUtils.java
+++ src/org/lineageos/lineageparts/style/util/AccentUtils.java
@@ -29,6 +29,8 @@ import org.lineageos.lineageparts.R;
 import org.lineageos.lineageparts.style.models.Accent;
 import org.lineageos.lineageparts.style.models.StyleStatus;
 
+import lineageos.style.StyleInterface;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -92,7 +94,7 @@ public final class AccentUtils {
     @NonNull
     private static Accent getDefaultAccent(Context context) {
         return new Accent(context.getString(R.string.style_accent_default_name),
-                "", Color.parseColor("#167C80"), StyleStatus.DYNAMIC);
+                StyleInterface.ACCENT_DEFAULT, Color.parseColor("#167C80"), StyleStatus.DYNAMIC);
     }
 
     public static boolean isCompatible(StyleStatus currentStatus, Accent accent) {
