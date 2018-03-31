5852679fb8f3188bbdab62e3b2a279f561baa894
Bruno Martins <bgcngm@gmail.com>
2018-03-31 13:50:52 +0100

    LineageParts: Build with AAPT2
    Change-Id: I25f1c8022db3be527bfa49478f287c53cba8d36c

--- Android.mk
+++ Android.mk
@@ -4,27 +4,25 @@ include $(CLEAR_VARS)
 
 LOCAL_PACKAGE_NAME := LineageParts
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    android-support-v4 \
-    android-support-v13 \
+
+LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-dynamic-animation \
-    android-support-v7-recyclerview \
+    android-support-v4 \
+    android-support-v7-appcompat \
     android-support-v7-palette \
     android-support-v7-preference \
-    android-support-v7-appcompat \
-    android-support-v14-preference \
+    android-support-v7-recyclerview \
+    android-support-v13 \
+    android-support-v14-preference
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
     jsr305 \
     org.lineageos.platform.internal
 
 LOCAL_RESOURCE_DIR := \
-    $(LOCAL_PATH)/res \
-    frameworks/support/v7/preference/res \
-    frameworks/support/v14/preference/res \
-    frameworks/support/v7/appcompat/res \
-    frameworks/support/v7/recyclerview/res
+    $(LOCAL_PATH)/res
 
-LOCAL_AAPT_FLAGS := --auto-add-overlay \
-    --extra-packages android.support.v7.preference:android.support.v14.preference:android.support.v17.preference:android.support.v7.appcompat:android.support.v7.recyclerview
+LOCAL_USE_AAPT2 := true
 
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
