314b714fe83b4a1bb7c834dee6bd5f8d64ce9cf2
Rashed Abdel-Tawab <rashed@linux.com>
2018-05-14 14:35:32 -0700

    LineageParts: Add reader mode frontend
    Change-Id: I36e473ccd30be6799a1cff797d6b7dd63fbb2db5

--- src/org/lineageos/lineageparts/livedisplay/LiveDisplaySettings.java
+++ src/org/lineageos/lineageparts/livedisplay/LiveDisplaySettings.java
@@ -53,6 +53,7 @@ import static lineageos.hardware.LiveDisplayManager.FEATURE_COLOR_ADJUSTMENT;
 import static lineageos.hardware.LiveDisplayManager.FEATURE_COLOR_ENHANCEMENT;
 import static lineageos.hardware.LiveDisplayManager.FEATURE_DISPLAY_MODES;
 import static lineageos.hardware.LiveDisplayManager.FEATURE_PICTURE_ADJUSTMENT;
+import static lineageos.hardware.LiveDisplayManager.FEATURE_READING_ENHANCEMENT;
 import static lineageos.hardware.LiveDisplayManager.MODE_OFF;
 import static lineageos.hardware.LiveDisplayManager.MODE_OUTDOOR;
 
@@ -67,6 +68,7 @@ public class LiveDisplaySettings extends SettingsPreferenceFragment implements S
     private static final String KEY_LIVE_DISPLAY = "live_display";
     private static final String KEY_LIVE_DISPLAY_AUTO_OUTDOOR_MODE =
             "display_auto_outdoor_mode";
+    private static final String KEY_LIVE_DISPLAY_READING_ENHANCEMENT = "display_reading_mode";
     private static final String KEY_LIVE_DISPLAY_LOW_POWER = "display_low_power";
     private static final String KEY_LIVE_DISPLAY_COLOR_ENHANCE = "display_color_enhance";
     private static final String KEY_LIVE_DISPLAY_TEMPERATURE = "live_display_color_temperature";
@@ -94,6 +96,7 @@ public class LiveDisplaySettings extends SettingsPreferenceFragment implements S
     private SwitchPreference mColorEnhancement;
     private SwitchPreference mLowPower;
     private SwitchPreference mOutdoorMode;
+    private SwitchPreference mReadingMode;
 
     private PictureAdjustment mPictureAdjustment;
     private DisplayTemperature mDisplayTemperature;
@@ -182,6 +185,15 @@ public class LiveDisplaySettings extends SettingsPreferenceFragment implements S
                 && !mConfig.hasFeature(MODE_OUTDOOR)) {
             liveDisplayPrefs.removePreference(mOutdoorMode);
             mOutdoorMode = null;
+        }
+
+        mReadingMode = (SwitchPreference) findPreference(KEY_LIVE_DISPLAY_READING_ENHANCEMENT);
+        if (liveDisplayPrefs != null && mReadingMode != null
+                && !mHardware.isSupported(LineageHardwareManager.FEATURE_READING_ENHANCEMENT)) {
+            liveDisplayPrefs.removePreference(mReadingMode);
+            mReadingMode = null;
+        } else {
+            mReadingMode.setOnPreferenceChangeListener(this);
         }
 
         mLowPower = (SwitchPreference) findPreference(KEY_LIVE_DISPLAY_LOW_POWER);
@@ -334,6 +346,8 @@ public class LiveDisplaySettings extends SettingsPreferenceFragment implements S
                     break;
                 }
             }
+        } else if (preference == mReadingMode) {
+            mHardware.setGrayscale((Boolean) objValue);
         }
         return true;
     }
