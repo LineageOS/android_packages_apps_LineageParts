09b099e5b84fc044af621bcf53ff95ca14b12b7a
Michael W <baddaemon87@gmail.com>
2018-08-03 20:35:24 +0200

    LineageParts: (Not-so-)Small cleanup
    * Remove unused imports
    * Fix a few line length issues
    * Convert a few anonymous classes to lambda expressions
    * Convert a few global vars to locals
    * Remove unused methods
    * Add copyrights to changed files
    
    Change-Id: I3187f135796b1a6a55de64defe7ef642396aadba

--- src/org/lineageos/lineageparts/utils/DeviceUtils.java
+++ src/org/lineageos/lineageparts/utils/DeviceUtils.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2016 The CyanogenMod project
- * Copyright (C) 2017 The LineageOS project
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -104,7 +104,8 @@ public class DeviceUtils {
     }
 
     public static boolean deviceSupportsMobileData(Context ctx) {
-        ConnectivityManager cm = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
+        ConnectivityManager cm =
+                (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
         return cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE);
     }
 
