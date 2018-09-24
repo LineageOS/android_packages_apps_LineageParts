bca0966b71999e13bfe6241d91a6dbc3d0282b65
Danny Baumann <dannybaumann@web.de>
2018-09-24 12:14:07 +0200

    LineageParts: Re-enable expanded desktop.
    Change-Id: I4f3315b6987b9400d1975df95eb67110f9a86065

--- src/org/lineageos/lineageparts/applications/ExpandedDesktopSettings.java
+++ src/org/lineageos/lineageparts/applications/ExpandedDesktopSettings.java
@@ -33,7 +33,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManagerGlobal;
-/*
 import android.view.WindowManagerPolicyControl;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
@@ -100,8 +99,7 @@ public class ExpandedDesktopSettings extends SettingsPreferenceFragment
 
         mIsGloballyExpanded = isGloballyExpanded(getActivity().getContentResolver());
         if (!mIsGloballyExpanded) {
-            WindowManagerPolicyControl.reloadFromSetting(getActivity(),
-                    Settings.Global.POLICY_CONTROL);
+            WindowManagerPolicyControl.reloadFromSetting(getActivity());
         }
         mAllPackagesAdapter = new AllPackagesAdapter(getActivity());
     }
@@ -319,8 +317,7 @@ public class ExpandedDesktopSettings extends SettingsPreferenceFragment
 
     private void save() {
         if (!mIsGloballyExpanded) {
-            WindowManagerPolicyControl.saveToSettings(getActivity(),
-                    Settings.Global.POLICY_CONTROL);
+            WindowManagerPolicyControl.saveToSettings(getActivity());
         }
     }
 
@@ -453,7 +450,6 @@ public class ExpandedDesktopSettings extends SettingsPreferenceFragment
             }
 
             final int index = Arrays.binarySearch(mPositions, position);
-*/
 
             /*
              * Consider this example: section positions are 0, 3, 5; the supplied
@@ -463,7 +459,6 @@ public class ExpandedDesktopSettings extends SettingsPreferenceFragment
              * To get from that number to the expected value of 1 we need to negate
              * and subtract 2.
              */
-/*
             return index >= 0 ? index : -index - 2;
         }
 
@@ -591,4 +586,4 @@ public class ExpandedDesktopSettings extends SettingsPreferenceFragment
         }
     }
 }
-*/
+
