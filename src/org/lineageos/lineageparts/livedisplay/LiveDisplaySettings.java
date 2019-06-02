5a4941f97c304befc88bc3277204294633ec167e
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-06-02 18:32:11 -0300

    Settings: Don't index display mode and color temperature on HWC2
    Change-Id: Iec23d14c6a7256432abc9a9bc313c303b88178c5

--- src/org/lineageos/lineageparts/livedisplay/LiveDisplaySettings.java
+++ src/org/lineageos/lineageparts/livedisplay/LiveDisplaySettings.java
@@ -405,6 +405,10 @@ public class LiveDisplaySettings extends SettingsPreferenceFragment implements S
             if (!config.hasFeature(FEATURE_READING_ENHANCEMENT)) {
                 result.add(KEY_LIVE_DISPLAY_READING_ENHANCEMENT);
             }
+            if (ColorDisplayController.isAvailable(context)) {
+                result.add(KEY_LIVE_DISPLAY);
+                result.add(KEY_LIVE_DISPLAY_TEMPERATURE);
+            }
             return result;
         }
 
