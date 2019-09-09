a8b5e3020ac798fddb370d883db1ac4b8eddae21
LuK1337 <priv.luk@gmail.com>
2019-09-09 12:38:46 +0200

    LineageParts: Reenable PerfProfileSettings
    Change-Id: I329b0b94477cc47762ff8454179e59d61ece9491

--- src/org/lineageos/lineageparts/power/PerfProfileSettings.java
+++ src/org/lineageos/lineageparts/power/PerfProfileSettings.java
@@ -232,14 +232,12 @@ public class PerfProfileSettings extends SettingsPreferenceFragment
                 return false;
             }
         } else if (preference == mPowerSavePref) {
-/* setPowerSaveMode
             if (!mPowerManager.setPowerSaveMode((boolean) newValue)) {
                 // Don't just fail silently, inform the user as well
                 Toast.makeText(getActivity(),
                         R.string.perf_profile_fail_toast, Toast.LENGTH_SHORT).show();
                 return false;
             }
-*/
             updatePowerSaveValue();
         } else if (preference == mAutoPowerSavePref) {
             final int level = Integer.parseInt((String) newValue);
