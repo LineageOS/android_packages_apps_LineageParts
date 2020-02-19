3e3c630adcb1acdea7abb66a5321160dbbb57ccf
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-02-19 12:37:56 +0100

    Show "No apps" if no apps can be forced to full screen aspect ratio
    Change-Id: I9cebf8a90d35924d3947377549ac5ed8a76ac9c9

--- src/org/lineageos/lineageparts/applications/LongScreenSettings.java
+++ src/org/lineageos/lineageparts/applications/LongScreenSettings.java
@@ -90,6 +90,7 @@ public class LongScreenSettings extends SettingsPreferenceFragment
 
         mUserListView = (ListView) view.findViewById(R.id.user_list_view);
         mUserListView.setAdapter(mAllPackagesAdapter);
+        mUserListView.setEmptyView(view.findViewById(R.id.user_list_empty_view));
     }
 
     @Override
