5a88bedf7d9d184af0b9ce640a2f3b7f54a4d354
Gegham Zakaryan <zakaryan.2004@outlook.com>
2021-04-04 15:45:42 +0400

    PowerMenuActions: Add more checks for bugreport and lockdown option states
    Use LockPatternUtils to check if lock screen is secure.
    Use UserManager and UserInfo to check if the current user is primary.
    
    In result, bugreport and lockdown are visible and toggled only during correct scenarios:
    	- Bug reporting option is disabled if development settings aren't on
    	- Bug reporting option is disabled if the current user isn't the primary user
    	- Bug reporting checkbox reflects the state of Settings.Global.BUGREPORT_IN_POWER_MENU
    	- Lockdown option is disabled if keyguard isn't secure
    	- Lockdown checkbox reflects the state of Settings.Secure.LOCKDOWN_IN_POWER_MENU
    
    String summaries are added for each scenario.
    
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
    Change-Id: I8a4e4a6078e8277ce2cfee16d5b2abb4de91c746

--- src/org/lineageos/lineageparts/input/PowerMenuActions.java
+++ src/org/lineageos/lineageparts/input/PowerMenuActions.java
@@ -27,6 +27,8 @@ import android.provider.Settings;
 import androidx.preference.CheckBoxPreference;
 import androidx.preference.Preference;
 
+import com.android.internal.widget.LockPatternUtils;
+
 import org.lineageos.internal.util.PowerMenuConstants;
 import org.lineageos.lineageparts.R;
 import org.lineageos.lineageparts.SettingsPreferenceFragment;
@@ -51,6 +53,8 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
     private LineageGlobalActions mLineageGlobalActions;
 
     Context mContext;
+    private LockPatternUtils mLockPatternUtils;
+    private UserManager mUserManager;
     private List<String> mLocalUserConfig = new ArrayList<String>();
 
     @Override
@@ -59,6 +63,8 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
 
         addPreferencesFromResource(R.xml.power_menu_settings);
         mContext = getActivity().getApplicationContext();
+        mLockPatternUtils = new LockPatternUtils(mContext);
+        mUserManager = UserManager.get(mContext);
         mLineageGlobalActions = LineageGlobalActions.getInstance(mContext);
 
         for (String action : PowerMenuConstants.getAllActions()) {
@@ -97,8 +103,7 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
                 getPreferenceScreen().removePreference(findPreference(GLOBAL_ACTION_KEY_USERS));
                 mUsersPref = null;
             } else {
-                List<UserInfo> users = ((UserManager) mContext.getSystemService(
-                        Context.USER_SERVICE)).getUsers();
+                List<UserInfo> users = mUserManager.getUsers();
                 boolean enabled = (users.size() > 1);
                 mUsersPref.setChecked(mLineageGlobalActions.userConfigContains(
                         GLOBAL_ACTION_KEY_USERS) && enabled);
@@ -139,6 +144,8 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
         } else if (preference == mBugReportPref) {
             value = mBugReportPref.isChecked();
             mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_BUGREPORT);
+            Settings.Global.putInt(getContentResolver(),
+                    Settings.Global.BUGREPORT_IN_POWER_MENU, value ? 1 : 0);
 
         } else if (preference == mLockDownPref) {
             value = mLockDownPref.isChecked();
@@ -153,15 +160,38 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
     }
 
     private void updatePreferences() {
-        boolean bugreport = Settings.Global.getInt(getContentResolver(),
-                Settings.Global.BUGREPORT_IN_POWER_MENU, 0) != 0;
-
+        UserInfo currentUser = mUserManager.getUserInfo(UserHandle.myUserId());
+        boolean developmentSettings = Settings.Global.getInt(
+                getContentResolver(), Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0) == 1;
+        boolean bugReport = Settings.Global.getInt(
+                getContentResolver(), Settings.Global.BUGREPORT_IN_POWER_MENU, 0) == 1;
+        boolean isPrimaryUser = currentUser == null || currentUser.isPrimary();
         if (mBugReportPref != null) {
-            mBugReportPref.setEnabled(bugreport);
-            if (bugreport) {
-                mBugReportPref.setSummary(null);
+            mBugReportPref.setEnabled(developmentSettings && isPrimaryUser);
+            if (!developmentSettings) {
+                mBugReportPref.setChecked(false);
+                mBugReportPref.setSummary(R.string.power_menu_bug_report_devoptions_unavailable);
+            } else if (!isPrimaryUser) {
+                mBugReportPref.setChecked(false);
+                mBugReportPref.setSummary(R.string.power_menu_bug_report_unavailable_for_user);
             } else {
-                mBugReportPref.setSummary(R.string.power_menu_bug_report_disabled);
+                mBugReportPref.setChecked(bugReport);
+                mBugReportPref.setSummary(null);
+            }
+        }
+
+        boolean isKeyguardSecure = mLockPatternUtils.isSecure(UserHandle.myUserId());
+        boolean lockdown = Settings.Secure.getIntForUser(
+                getContentResolver(), Settings.Secure.LOCKDOWN_IN_POWER_MENU, 0,
+                UserHandle.USER_CURRENT) == 1;
+        if (mLockDownPref != null) {
+            mLockDownPref.setEnabled(isKeyguardSecure);
+            if (isKeyguardSecure) {
+                mLockDownPref.setChecked(lockdown);
+                mLockDownPref.setSummary(null);
+            } else {
+                mLockDownPref.setChecked(false);
+                mLockDownPref.setSummary(R.string.power_menu_lockdown_unavailable);
             }
         }
     }
