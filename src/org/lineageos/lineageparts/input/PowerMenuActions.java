4f2ccb803b67bce7e802c7c956e7f07e2a4b9b54
LuK1337 <priv.luk@gmail.com>
2021-10-09 18:51:38 +0200

    LineageParts: Remove lockdown from power menu actions
    Change-Id: Icac473f63e82de1a8e34275a6d70668291b03887

--- src/org/lineageos/lineageparts/input/PowerMenuActions.java
+++ src/org/lineageos/lineageparts/input/PowerMenuActions.java
@@ -48,7 +48,6 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
     private CheckBoxPreference mAirplanePref;
     private CheckBoxPreference mUsersPref;
     private CheckBoxPreference mBugReportPref;
-    private CheckBoxPreference mLockDownPref;
     private CheckBoxPreference mEmergencyPref;
 
     private LineageGlobalActions mLineageGlobalActions;
@@ -77,8 +76,6 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
                 mUsersPref = findPreference(GLOBAL_ACTION_KEY_USERS);
             } else if (action.equals(GLOBAL_ACTION_KEY_BUGREPORT)) {
                 mBugReportPref = findPreference(GLOBAL_ACTION_KEY_BUGREPORT);
-            } else if (action.equals(GLOBAL_ACTION_KEY_LOCKDOWN)) {
-                mLockDownPref = findPreference(GLOBAL_ACTION_KEY_LOCKDOWN);
             } else if (action.equals(GLOBAL_ACTION_KEY_EMERGENCY)) {
                 mEmergencyPref = findPreference(GLOBAL_ACTION_KEY_EMERGENCY);
             }
@@ -155,12 +152,6 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
             Settings.Global.putInt(getContentResolver(),
                     Settings.Global.BUGREPORT_IN_POWER_MENU, value ? 1 : 0);
 
-        } else if (preference == mLockDownPref) {
-            value = mLockDownPref.isChecked();
-            mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_LOCKDOWN);
-            Settings.Secure.putIntForUser(getContentResolver(),
-                    Settings.Secure.LOCKDOWN_IN_POWER_MENU, value ? 1 : 0, UserHandle.USER_CURRENT);
-
         } else if (preference == mEmergencyPref) {
             value = mEmergencyPref.isChecked();
             mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_EMERGENCY);
@@ -189,21 +180,6 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
             } else {
                 mBugReportPref.setChecked(bugReport);
                 mBugReportPref.setSummary(null);
-            }
-        }
-
-        boolean isKeyguardSecure = mLockPatternUtils.isSecure(UserHandle.myUserId());
-        boolean lockdown = Settings.Secure.getIntForUser(
-                getContentResolver(), Settings.Secure.LOCKDOWN_IN_POWER_MENU, 0,
-                UserHandle.USER_CURRENT) == 1;
-        if (mLockDownPref != null) {
-            mLockDownPref.setEnabled(isKeyguardSecure);
-            if (isKeyguardSecure) {
-                mLockDownPref.setChecked(lockdown);
-                mLockDownPref.setSummary(null);
-            } else {
-                mLockDownPref.setChecked(false);
-                mLockDownPref.setSummary(R.string.power_menu_lockdown_unavailable);
             }
         }
     }
