2abaf17ac8adc0c541058a185508d2d56d377ad6
Bruno Martins <bgcngm@gmail.com>
2022-01-01 23:50:49 +0000

    parts: Update SettingsPreferenceFragment with latest AOSP changes
    This obviously has repercussions in a lot of other classes due to
    the full migration to androidx.
    
    Change-Id: Ib8cfade5f9b958e599cece4360482ad38f4de1ed

--- src/org/lineageos/lineageparts/profiles/triggers/NfcTriggerFragment.java
+++ src/org/lineageos/lineageparts/profiles/triggers/NfcTriggerFragment.java
@@ -16,7 +16,6 @@
  */
 package org.lineageos.lineageparts.profiles.triggers;
 
-import android.app.Fragment;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -27,6 +26,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import lineageos.app.Profile;
 
