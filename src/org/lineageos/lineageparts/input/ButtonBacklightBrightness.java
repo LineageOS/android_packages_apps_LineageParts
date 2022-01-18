f47931fe8903bbcedf1a9edcf9087ab49d0eaac7
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2022-01-18 13:35:26 +0100

    ButtonBacklightBrightness: Fix custom alert dialogs
    Change-Id: I9f0a2ceda6c3b3296dd8dc264821dc97541bcd5a

--- src/org/lineageos/lineageparts/input/ButtonBacklightBrightness.java
+++ src/org/lineageos/lineageparts/input/ButtonBacklightBrightness.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2013 The CyanogenMod Project
- *               2017-2020 The LineageOS Project
+ *               2017-2022 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -112,6 +112,18 @@ public class ButtonBacklightBrightness extends CustomDialogPreference<AlertDialo
             public void onClick(DialogInterface dialog, int which) {
             }
         });
+        builder.setNegativeButton(R.string.cancel,
+                new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        builder.setPositiveButton(R.string.dlg_ok,
+                new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });    
     }
 
     @Override
