f62f006e034f8f69ec653e00aa128c51f035b94d
Michael Bestas <mkbestas@lineageos.org>
2023-03-15 02:58:27 +0200

    TMP fix for volume_link_notification removal
    Change-Id: I83ceac92fe5b08ce4a0e1d76266e5f0cfadf5ac2

--- src/org/lineageos/lineageparts/profiles/SeekBarVolumizer.java
+++ src/org/lineageos/lineageparts/profiles/SeekBarVolumizer.java
@@ -230,7 +230,7 @@ public class SeekBarVolumizer implements OnSeekBarChangeListener, Handler.Callba
 
     private boolean isNotificationStreamLinked() {
         return mVoiceCapable && Settings.Secure.getInt(mContext.getContentResolver(),
-                Settings.Secure.VOLUME_LINK_NOTIFICATION, 1) == 1;
+                "volume_link_notification", 1) == 1;
     }
 
     public void setSeekBar(SeekBar seekBar) {
