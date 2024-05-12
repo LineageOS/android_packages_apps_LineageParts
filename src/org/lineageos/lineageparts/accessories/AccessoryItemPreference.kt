a621b8b23be6fb586ebae0c1aa091874d2d25c6e
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-05-12 23:57:00 +0200

    LineageParts: Initial accessories support
    Change-Id: I2b104222ba228f8771aab2dc8b15153e9bab3c3a

---
+++ src/org/lineageos/lineageparts/accessories/AccessoryItemPreference.kt
@@ -0,0 +1,67 @@
+/*
+ * SPDX-FileCopyrightText: 2024 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.lineageparts.accessories
+
+import android.content.Context
+import androidx.preference.Preference
+import lineageos.accessories.AccessoryInfo
+import lineageos.accessories.AccessoryType
+import lineageos.accessories.BatteryInfo
+import lineageos.accessories.IAccessory
+import lineageos.accessories.IAccessoryCallback
+import org.lineageos.lineageparts.R
+
+class AccessoryItemPreference(
+    context: Context,
+    private val accessory: IAccessory,
+    accessoryId: String,
+) : Preference(context) {
+    private val accessoryCallback = object : IAccessoryCallback.Stub() {
+        override fun onAccessoryInfoUpdated(accessoryInfo: AccessoryInfo?) {
+            accessoryInfo?.let {
+                title = it.displayName
+                summary = it.displayId
+                setIcon(
+                    when (it.type) {
+                        AccessoryType.KEYBOARD -> R.drawable.ic_keyboard
+                        AccessoryType.ACTIVE_PEN -> R.drawable.ic_ink_pen
+                        else -> R.drawable.ic_question_mark
+                    }
+                )
+            }
+        }
+
+        override fun onAccessoryStatusUpdated(status: Byte) {
+
+        }
+
+        override fun onBatteryInfoUpdated(batteryInfo: BatteryInfo?) {
+
+        }
+    }
+
+    init {
+        key = accessoryId
+
+        setOnPreferenceClickListener {
+            // TODO open accessory settings
+
+            true
+        }
+    }
+
+    override fun onAttached() {
+        super.onAttached()
+
+        accessory.registerCallback(accessoryCallback)
+    }
+
+    override fun onDetached() {
+        super.onDetached()
+
+        accessory.unregisterCallback(accessoryCallback)
+    }
+}
