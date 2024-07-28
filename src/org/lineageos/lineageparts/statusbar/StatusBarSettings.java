6e6d736906fd4bbacf73c236c08454c48ae3bfed
Bruno Martins <bgcngm@gmail.com>
2024-07-28 14:39:57 +0100

    StatusBarSettings: Hide battery category on battery-less devices
    Change-Id: I37cb30eae68a80a2b7d8e5c5a645ca4266ef4cf4

--- src/org/lineageos/lineageparts/statusbar/StatusBarSettings.java
+++ src/org/lineageos/lineageparts/statusbar/StatusBarSettings.java
@@ -1,10 +1,12 @@
 /*
  * SPDX-FileCopyrightText: 2014-2015 The CyanogenMod Project
- * SPDX-FileCopyrightText: 2017-2023 The LineageOS Project
+ * SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 package org.lineageos.lineageparts.statusbar;
 
+import android.content.Intent;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.text.TextUtils;
@@ -13,6 +15,8 @@ import android.view.View;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
+
+import com.android.settingslib.fuelgauge.BatteryUtils;
 
 import org.lineageos.lineageparts.R;
 import org.lineageos.lineageparts.SettingsPreferenceFragment;
@@ -51,6 +55,8 @@ public class StatusBarSettings extends SettingsPreferenceFragment
     private PreferenceCategory mStatusBarBatteryCategory;
     private PreferenceCategory mStatusBarClockCategory;
 
+    private boolean mBatteryPresent;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,6 +74,10 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         statusBarBattery.setOnPreferenceChangeListener(this);
         enableStatusBarBatteryDependents(statusBarBattery.getIntValue(2));
 
+        Intent intent = BatteryUtils.getBatteryIntent(getContext());
+        if (intent != null) {
+            mBatteryPresent = intent.getBooleanExtra(BatteryManager.EXTRA_PRESENT, true);
+        }
         mStatusBarBatteryCategory = getPreferenceScreen().findPreference(CATEGORY_BATTERY);
 
         mQuickPulldown = findPreference(STATUS_BAR_QUICK_QS_PULLDOWN);
@@ -88,7 +98,8 @@ public class StatusBarSettings extends SettingsPreferenceFragment
             getPreferenceScreen().addPreference(mStatusBarClockCategory);
         }
 
-        if (TextUtils.delimitedStringContains(curIconBlacklist, ',', "battery")) {
+        if (!mBatteryPresent ||
+                TextUtils.delimitedStringContains(curIconBlacklist, ',', "battery")) {
             getPreferenceScreen().removePreference(mStatusBarBatteryCategory);
         } else {
             getPreferenceScreen().addPreference(mStatusBarBatteryCategory);
