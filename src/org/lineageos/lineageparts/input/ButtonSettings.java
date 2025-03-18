dae6faa24ef37c66efa4344d192b69b04c065abd
LuK1337 <priv.luk@gmail.com>
2025-03-18 20:36:33 +0100

    LineageParts: Get rid of 2 button nav leftovers
    Change-Id: I499f026ca997434639105ea982b7eec96750cbcb

--- src/org/lineageos/lineageparts/input/ButtonSettings.java
+++ src/org/lineageos/lineageparts/input/ButtonSettings.java
@@ -6,7 +6,6 @@
 
 package org.lineageos.lineageparts.input;
 
-import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_2BUTTON;
 import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_3BUTTON_OVERLAY;
 import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_GESTURAL_OVERLAY;
 
@@ -655,20 +654,11 @@ public class ButtonSettings extends SettingsPreferenceFragment
             return true;
         } else if (preference == mEnableTaskbar) {
             toggleTaskBarDependencies((Boolean) newValue);
-            if ((Boolean) newValue && is2ButtonNavigationEnabled(requireContext())) {
-                // Let's switch to gestural mode if user previously had 2 buttons enabled.
-                setButtonNavigationMode(NAV_BAR_MODE_GESTURAL_OVERLAY);
-            }
             LineageSettings.System.putInt(getContentResolver(),
                     LineageSettings.System.ENABLE_TASKBAR, ((Boolean) newValue) ? 1 : 0);
             return true;
         }
         return false;
-    }
-
-    private static boolean is2ButtonNavigationEnabled(Context context) {
-        return NAV_BAR_MODE_2BUTTON == context.getResources().getInteger(
-                com.android.internal.R.integer.config_navBarInteractionMode);
     }
 
     private static void setButtonNavigationMode(String overlayPackage) {
@@ -744,13 +734,6 @@ public class ButtonSettings extends SettingsPreferenceFragment
                     mNavigationPreferencesCat.removePreference(mNavigationHomeLongPressAction);
                     mNavigationPreferencesCat.removePreference(mNavigationHomeDoubleTapAction);
                     mNavigationPreferencesCat.removePreference(mNavigationAppSwitchLongPressAction);
-                } else if (DeviceUtils.isSwipeUpEnabled(getContext())) {
-                    mNavigationPreferencesCat.addPreference(mNavigationBackLongPressAction);
-                    mNavigationPreferencesCat.addPreference(mNavigationHomeLongPressAction);
-                    mNavigationPreferencesCat.addPreference(mNavigationHomeDoubleTapAction);
-
-                    mNavigationPreferencesCat.removePreference(mNavigationAppSwitchLongPressAction);
-                    mNavigationPreferencesCat.removePreference(mEdgeLongSwipeAction);
                 } else {
                     mNavigationPreferencesCat.addPreference(mNavigationBackLongPressAction);
                     mNavigationPreferencesCat.addPreference(mNavigationHomeLongPressAction);
@@ -998,9 +981,6 @@ public class ButtonSettings extends SettingsPreferenceFragment
                     result.add(KEY_NAVIGATION_HOME_LONG_PRESS);
                     result.add(KEY_NAVIGATION_HOME_DOUBLE_TAP);
                     result.add(KEY_NAVIGATION_APP_SWITCH_LONG_PRESS);
-                } else if (DeviceUtils.isSwipeUpEnabled(context)) {
-                    result.add(KEY_NAVIGATION_APP_SWITCH_LONG_PRESS);
-                    result.add(KEY_EDGE_LONG_SWIPE);
                 } else {
                     result.add(KEY_EDGE_LONG_SWIPE);
                 }
