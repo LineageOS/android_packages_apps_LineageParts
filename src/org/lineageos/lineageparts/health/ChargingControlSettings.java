8f1563983bc369a954ec9222b003979c55acc73e
Bruno Martins <bgcngm@gmail.com>
2025-11-23 11:35:23 +0000

    ChargingControlSettings: Ditch DropDownPreference and use ListPreference
    This custom and quite old preference type is only used here,
    let's just stop using it.
    
    Change-Id: Ib4a82e9c66e5d8606ad8845fa71eeb894b535a94

--- src/org/lineageos/lineageparts/health/ChargingControlSettings.java
+++ src/org/lineageos/lineageparts/health/ChargingControlSettings.java
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-FileCopyrightText: 2023-2025 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -21,7 +21,7 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 
 import lineageos.health.HealthInterface;
-import lineageos.preference.LineageSystemSettingDropDownPreference;
+import lineageos.preference.LineageSystemSettingListPreference;
 import lineageos.preference.LineageSystemSettingMainSwitchPreference;
 import lineageos.providers.LineageSettings;
 
@@ -47,7 +47,7 @@ public class ChargingControlSettings extends SettingsPreferenceFragment implemen
     private static final String CHARGING_CONTROL_LIMIT_PREF = "charging_control_charging_limit";
 
     private LineageSystemSettingMainSwitchPreference mChargingControlEnabledPref;
-    private LineageSystemSettingDropDownPreference mChargingControlModePref;
+    private LineageSystemSettingListPreference mChargingControlModePref;
     private StartTimePreference mChargingControlStartTimePref;
     private TargetTimePreference mChargingControlTargetTimePref;
     private ChargingLimitPreference mChargingControlLimitPref;
