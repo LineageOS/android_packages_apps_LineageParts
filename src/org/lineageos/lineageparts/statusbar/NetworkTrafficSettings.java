29edafce0ef3842f80fa79f66c4a217dcab61cfc
grepfox <grepfox@tutamail.com>
2026-02-11 19:04:16 +0530

    LineageParts: allow hiding arrows in network traffic [2/3]
    Change-Id: Ifde5ddd3b5579d106323895ae1936094e41bc3c2

--- src/org/lineageos/lineageparts/statusbar/NetworkTrafficSettings.java
+++ src/org/lineageos/lineageparts/statusbar/NetworkTrafficSettings.java
@@ -42,6 +42,7 @@ public class NetworkTrafficSettings extends SettingsPreferenceFragment
     private ListPreference mNetTrafficMode;
     private ListPreference mNetTrafficPosition;
     private LineageSecureSettingSwitchPreference mNetTrafficAutohide;
+    private LineageSecureSettingSwitchPreference mNetTrafficHideArrows;
     private ListPreference mNetTrafficUnits;
     private ListPreference mNetTrafficShowUnits;
 
@@ -96,6 +97,9 @@ public class NetworkTrafficSettings extends SettingsPreferenceFragment
 
         mNetTrafficAutohide = findPreference(LineageSettings.Secure.NETWORK_TRAFFIC_AUTOHIDE);
         mNetTrafficAutohide.setOnPreferenceChangeListener(this);
+
+        mNetTrafficHideArrows = findPreference(LineageSettings.Secure.NETWORK_TRAFFIC_HIDE_ARROWS);
+        mNetTrafficHideArrows.setOnPreferenceChangeListener(this);
 
         mNetTrafficUnits = findPreference(LineageSettings.Secure.NETWORK_TRAFFIC_UNITS);
         mNetTrafficUnits.setOnPreferenceChangeListener(this);
@@ -171,6 +175,7 @@ public class NetworkTrafficSettings extends SettingsPreferenceFragment
         final boolean enabled = mode != 0;
         mNetTrafficPosition.setEnabled(enabled);
         mNetTrafficAutohide.setEnabled(enabled);
+        mNetTrafficHideArrows.setEnabled(enabled);
         mNetTrafficUnits.setEnabled(enabled);
         mNetTrafficShowUnits.setEnabled(enabled);
     }
